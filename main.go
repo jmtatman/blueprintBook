2728cab4bf7e2c96bd5cfb1e08c4594553cc93d3
tatman <tatman@oath.com>
2019-04-08 21:13:12 -0400

    slowly getting to the actual login portion

--- main.go
+++ main.go
@@ -28,6 +28,7 @@ func main() {
 	r := newRoom()
 	//root
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.Handle("/room", r)
 	go r.run()
 	//start server
